af1e81adb16ff562b6040ed1b4a659b251e2a06b
Qian <161250102@smail.nju.edu.cn>
2018-10-30 20:41:57 +0800

    Change comment style and __repr__ method

--- train/models.py
+++ train/models.py
@@ -59,12 +59,13 @@ class Station(BaseModel):
 
 
 class StationInfo(object):
-    """
-    车站信息
-    :param name: 中文名
-    :param pinyin: 拼音
-    :param code_name: 英文代号
-    :param abb_pinyin: 拼音首字母缩写
+    """车站信息
+
+    Attributes:
+        name: 中文名
+        pinyin: 拼音
+        tele_code: 电报码
+        pinyin_code: 拼音首字母缩写
     """
 
     def __init__(self, name, pinyin, tele_code, pinyin_code):
@@ -78,12 +79,13 @@ class StationInfo(object):
 
 
 class TrainInfo(object):
-    """
-    车次信息，不包含详细的列车时刻表
-    :param code: 车次
-    :param start: 始发站[中文]
-    :param end  终到站[中文]
-    :param train_no: 车次编号
+    """车次信息，不包含详细的列车时刻表
+
+    Attributes:
+        code: 车次
+        start: 始发站[中文]
+        end  终到站[中文]
+        train_no: 车次编号
     """
 
     def __init__(self, code, start, end, train_no):
@@ -97,11 +99,12 @@ class TrainInfo(object):
 
 
 class TrainDetail(object):
-    """
-    某一站的停靠信息
-    :param station_name: 车站中文名
-    :param arrive_time: 到达时间[None表示为始发站]
-    :param start_time: 开车时间[None表示为终到站]
+    """某一站的停靠信息
+
+    Attributes:
+        station_name: 车站中文名
+        arrive_time: 到达时间[None表示为始发站]
+        start_time: 开车时间[None表示为终到站]
     """
 
     def __init__(self, station_name, arrive_time, start_time):
@@ -110,16 +113,17 @@ class TrainDetail(object):
         self.start_time = start_time
 
     def __repr__(self):
-        return ('{%s, %s, %s}' % (self.station_name, self.arrive_time, self.start_time)).encode('utf-8')
+        return '{%s, %s, %s}' % (self.station_name, self.arrive_time, self.start_time)
 
 
 class Train(object):
-    """
-    一趟完整的车次信息
-    code: 车次
-    start: 始发站[中文]
-    end  终到站[中文]
-    station_details: 停靠站点信息
+    """一趟完整的车次信息
+
+    Attributes:
+        code: 车次
+        start: 始发站[中文]
+        end  终到站[中文]
+        station_details: 停靠站点信息
     """
 
     def __init__(self, train_info, station_details):
@@ -129,4 +133,5 @@ class Train(object):
         self.station_details = station_details
 
     def __repr__(self):
-        return ("%s -- %s -- %s\n%s" % (self.start, self.code, self.end, self.station_details)).encode('utf-8')
+        details = [station_detail.__repr__() for station_detail in self.station_details]
+        return "%s -- %s -- %s\n%s" % (self.start, self.code, self.end, "\n".join(details))
